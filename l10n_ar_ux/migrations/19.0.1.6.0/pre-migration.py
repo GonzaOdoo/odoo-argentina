b77f85f084c2d11e2d8edd7be2ec5383f9ee81f8
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-06-12 22:32:22 -0300

    Added migration fix

--- l10n_ar_ux/migrations/19.0.1.6.0/pre-migration.py
+++ l10n_ar_ux/migrations/19.0.1.6.0/pre-migration.py
@@ -72,3 +72,10 @@ def migrate(cr, version):
                 """,
                     (xml_id_name,),
                 )
+    cr.execute("""
+        UPDATE l10n_latam_check
+           SET payment_date = create_date::date
+         WHERE id = 1400
+           AND payment_date IS NULL
+    """)
+    _logger.warning("Set payment_date to create_date for l10n_latam_check with id 1400")
